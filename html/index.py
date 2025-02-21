03400f1f14d7e8293e108c61df5d7b7fc7419a78
Melissa Woo <s224311@dtu.dk>
2025-02-21 16:44:08 +0100

    removing commented out database code again

--- html/index.py
+++ html/index.py
@@ -3,18 +3,6 @@ print("Content-Type: text/html")
 print("Cache-Control: no-cache")
 print("")
 
-#import sqlite3
-
-# Connect to the SQLite database
-#db_path = "../game.db"  
-#conn = sqlite3.connect(db_path)
-#cursor = conn.cursor()
-
-# Query the database
-#cursor.execute("SELECT userid FROM user") 
-#rows = cursor.fetchall()
-
-# Generate HTML content
 html_content = """<!DOCTYPE html>
 <html>
 <head>
@@ -28,12 +16,6 @@ html_content = """<!DOCTYPE html>
 </body>
 </html>"""
 
-# Save the HTML file in Apache's web directory
-# html_file_path = "/var/www/html/index.html"  # Apache serves from here by default
-# with open(html_file_path, "w") as file:
-#     file.write(html_content)
 
 print(html_content)
 
-# Close the database connection
-# conn.close()
