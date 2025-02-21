1a5e214f06c108177fbe57fd2d27dbaebf57d1b4
Melissa <s224311@dtu.dk>
2025-02-21 16:10:08 +0100

    adding headers and print single html content

--- html/test2.py
+++ html/test2.py
@@ -1,1 +1,14 @@
-print("hej")
+#!/bin/env python3
+print("Content-Type: text/html")
+print("Cache-Control: no-cache")
+print("")
+
+html_content = """<!DOCTYPE html>
+<html lang="en">
+<head><title>User Data</title></head>
+
+<body> <p> hi again </p>
+</body>
+</html>"""
+
+print(html_content)
