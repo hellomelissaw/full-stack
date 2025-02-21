e7ed507451111b63b0788786f78e83d8ed6122bd
Melissa Woo <s224311@dtu.dk>
2025-02-21 16:47:53 +0100

    adding execution permission to file

--- html/index.py
+++ html/index.py
@@ -3,6 +3,18 @@ print("Content-Type: text/html")
 print("Cache-Control: no-cache")
 print("")
 
+#import sqlite3
+
+# Connect to the SQLite database
+#db_path = "../game.db"  
+#conn = sqlite3.connect(db_path)
+#cursor = conn.cursor()
+
+# Query the database
+#cursor.execute("SELECT userid FROM user") 
+#rows = cursor.fetchall()
+
+# Generate HTML content
 html_content = """<!DOCTYPE html>
 <html>
 <head>
@@ -19,3 +31,5 @@ html_content = """<!DOCTYPE html>
 
 print(html_content)
 
+# Close the database connection
+# conn.close()
