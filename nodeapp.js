74106fd7b987b8d966f06966bfc6c4a49e844381
Melissa Woo <s224311@dtu.dk>
2025-02-28 11:37:54 +0100

    connecting to database and adding one test query to print in console

--- nodeapp.js
+++ nodeapp.js
@@ -1,7 +1,22 @@
+import sqlite3 from "sqlite3"
+
+const db = new sqlite3.Database(game.db)
+
 const url = require('node:url');
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
+
+sql = "SELECT * FROM user";
+params = [];
+
+db.all(sql, params, function(err, rows) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(rows);
+    }
+  });
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -13,3 +28,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+db.close();
