a3db9f76f83f09b0b50445a936e3f64973545e2f
Melissa Woo <s224311@dtu.dk>
2025-02-28 15:48:50 +0100

    adding test code to query database and show data in html table

--- nodeapp.js
+++ nodeapp.js
@@ -1,21 +1,43 @@
-const url = require('node:url');
 const http = require('http');
+const sqlite3 = require('sqlite3').verbose();
+
 const hostname = 'localhost';
 const port = 3000;
 
-const sqlite3 = require('sqlite3');
-// const { DatabaseSync } = require('sqlite3');
 const database = new sqlite3.Database('game.db');
 
-const query = database.prepare('SELECT * from user');
-
-console.log(query.all());
-
 const server = http.createServer((req, res) => {
+    // query database
+    database.all('SELECT * FROM user', (err, rows) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Hov! Database error: ' + err.message);
+            return;
+        }
+
+        // make html page with table data
+        let html = `
+            <html>
+                <head>
+                    <title> Users </title>
+                </head>
+
+                <body>
+                    <h1> User List </h1>
+                     <table>
+                        <tr><th>ID</th><th>Name</th></tr>
+                        ${rows.map(row => `<tr><td>${row.userid}</td><td>${row.name}</td></tr>`).join('')}
+                    </table>
+                </body>
+            </html>
+        `       
+    })
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Cache-Control', 'no-cache');
-    res.end('Welcome to da Game! hihi\n');
+    res.end(html);
 });
 
 server.listen(port, hostname, () => {
