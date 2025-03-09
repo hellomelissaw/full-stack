3df41a4006037525af4d3f928f7d36e065d2493a
Melissa Woo <s224311@dtu.dk>
2025-03-09 15:08:40 +0100

    removing unused code and error processing for sigterm

--- nodeapp.js
+++ nodeapp.js
@@ -19,8 +19,7 @@ const server = http.createServer(async(req, res) => {
     console.log("Creating server...");
     try {
         conn = await pool.getConnection();
-        // conn = pool.getConnection();
-        // const rows = await conn.query("SELECT * from Locations");
+
         const rows = await conn.query("SELECT * from Locations");
         console.log(rows);
         // make html page with table data
@@ -39,7 +38,7 @@ const server = http.createServer(async(req, res) => {
                 </body>
             </html>
         `;
-       console.log(html);
+
        res.statusCode = 200;
        res.setHeader('Content-Type', 'text/html');
        res.setHeader('Cache-Control', 'no-cache');
@@ -59,14 +58,7 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-process.on('SIGINT', () => {
-    console.log('Received SIGINT. Closing server and freeing port...');
-    server.close(() => {
-        console.log('HTTP server closed');
-        pool.end();  // Close database pool
-        process.exit(0);  // Exit the process
-    });
-});
+
 
 process.on('uncaughtException', (err) => {
     console.error('Uncaught exception: ', err);
@@ -76,58 +68,3 @@ process.on('uncaughtException', (err) => {
         process.exit(1);  // Exit the process with error code
     });
 });
-
-// asyncFunction().then(() => {
-//     pool.end();
-// });
-
-
-
-// const http = require('http');
-// const sqlite3 = require('sqlite3').verbose();
-
-// const hostname = 'localhost';
-// const port = 3000;
-
-// const database = new sqlite3.Database('game.db');
-
-// const server = http.createServer((req, res) => {
-//     // query database
-//     database.all('SELECT * FROM user', (err, rows) => {
-//         if (err) {
-//             res.statusCode = 500;
-//             res.setHeader('Content-Type', 'text/plain');
-//             res.end('Hov! Database error: ' + err.message);
-//             return;
-//         }
-
-//         // make html page with table data
-//         let html = `
-//             <html>
-//                 <head>
-//                     <title> Users </title>
-//                 </head>
-
-//                 <body>
-//                     <h1> User List </h1>
-//                      <table>
-//                         <tr><th>ID</th><th>Name</th></tr>
-//                         ${rows.map(row => `<tr><td>${row.userid}</td><td>${row.name}</td></tr>`).join('')}
-//                     </table>
-//                 </body>
-//             </html>
-//         `;
-        
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.setHeader('Cache-Control', 'no-cache');
-//         res.end(html);
-//     })
-
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-// // database.close();
