34cdc5763421f77fb9cac942e525ae01d3f73f8a
Melissa <s224311@dtu.dk>
2025-03-24 21:57:02 +0100

    removing logs

--- dataService.js
+++ dataService.js
@@ -50,11 +50,9 @@ async function updateUserLocation(conn, id, uid) {
 async function insertLocation(conn, name, emojis) {
     try{
         await conn.query("INSERT INTO location (name, emojis) VALUES (?, ?)", [name, emojis]);
-        console.log("inserted location");
         return 'Location inserted successfully!';
     
     } catch(err) {
-         console.log(`error: ${err.message}`);
          return err.message;
     }
    
