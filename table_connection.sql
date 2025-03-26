9ef3ba1cc9b399ee8ff9c48376ad420ae897c9c0
Melissa Woo <s224311@dtu.dk>
2025-03-26 20:52:39 +0100

    cleaning up unused files

--- table_connection.sql
+++
@@ -1,7 +0,0 @@
-CREATE TRIGGER insert_location_after_user
-AFTER INSERT ON user
-FOR EACH ROW
-BEGIN
-    INSERT INTO Location (description, userid) 
-    VALUES ('Default Location', NEW.userid);
-END;
