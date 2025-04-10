fd2893b3e00a9d9d87586f704c43cb7d4e494615
Melissa <s224311@dtu.dk>
2025-04-10 22:06:56 +0200

    adding missing arguments for userIsActive function

--- dataService.js
+++ dataService.js
@@ -40,7 +40,7 @@ async function userIsActive(conn, uid) {
 
 async function createSession(conn, sessionID, uid) {
     console.log(`sessionID: ${sessionID}, uid: ${uid}`);
-    if (await userIsActive()) {
+    if (await userIsActive(conn, uid)) {
         try {
             await conn.query('DELETE FROM session WHERE uid = ?', [uid]);
 
