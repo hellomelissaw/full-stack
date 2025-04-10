d3c83f32056d6e1419d8a6a2b5d86ebc143b51ca
Melissa <s224311@dtu.dk>
2025-04-10 11:19:40 +0200

    fixing variable names and indentations

--- routing.js
+++ routing.js
@@ -129,7 +129,7 @@ async function requestRoute(conn, req) {
             return loadGame(conn, parsedURL.query.pid);
 
         case '/new-game-page':
-            return pug.renderFile('./templates/new_game_form.pug', { uid: uid } );
+            return pug.renderFile('./templates/new_game_form.pug', { uid: parsedURL.query.uid } );
 
         case '/new-game':
             return createNewGame(conn, parsedURL.query.uid)
