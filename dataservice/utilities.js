5788d6aca95c2ad9a8c568655328464d77fbeba5
Melissa Woo <s224311@dtu.dk>
2025-05-07 11:23:19 -0400

    remove duplicate queries

--- dataservice/utilities.js
+++ dataservice/utilities.js
@@ -1,54 +1,11 @@
 ////////////////////////////////////////////////////////////
-// PREPARED QUERIES
+// QUERIES
 ////////////////////////////////////////////////////////////
-
-// const sql_conn = `SELECT
-//                 location_connection.loc_id,
-//                 location_connection.conn_id,
-//                 location.name AS conn_name
-//             FROM
-//                 location_connection
-//             JOIN 
-//                 location
-//             ON 
-//                 location_connection.conn_id = location.loc_id
-//             WHERE
-//                 location_connection.loc_id = ?`;
-
-// const update_player_loc_id = 'UPDATE player SET loc_id = ? WHERE pid = ?';
-
-// const create_player = 'INSERT INTO player (uid, name, loc_id) values (?, ?, ?)';
-
-// const create_session = 'INSERT INTO session (session_id, uid) values (?, ?)';
-
-// const add_pid_to_session = 'UPDATE session SET pid = ? WHERE uid = ?';
-
-// const get_user_players = `SELECT 
-//                             player.pid, 
-//                             player.name, 
-//                             location.name AS loc_name 
-//                         FROM 
-//                             location JOIN player
-//                         ON 
-//                             location.loc_id = player.loc_id
-//                         WHERE 
-//                             player.uid = ?
-//                         ORDER BY 
-//                             player.pid`;
 
 const update_password = `UPDATE user
                         SET password = ?
                         WHERE uid = ?`;
 
-// // TODO optimize for performance if future table becomes larger
-// const select_random_enemy = `SELECT * FROM enemy
-//                             ORDER BY RAND()
-//                             LIMIT 1;
-//                             `
-
-// const update_stats = `UPDATE player
-//                       SET health = ?, experience = ?, level = ?
-//                       WHERE pid = ?`;
 ////////////////////////////////////////////////////////////
 // GENERAL QUERYING
 ////////////////////////////////////////////////////////////
