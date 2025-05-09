6156f54c75d5eccfa3f2587b8c20d0c6442116fd
Melissa Woo <s224311@dtu.dk>
2025-05-09 10:50:56 +0200

    adding else statement to handle if getUserData success false:

--- routing/login-routes.js
+++ routing/login-routes.js
@@ -86,6 +86,10 @@ async function validateLoginResponse(conn, req, temp_token) {
                 message: 'Something went wrong when checking the password.'
             });
         }
+    } else {
+        return pug.renderFile('./templates/message.pug', {
+            message: 'Something went wrong with getting user data.'
+        });
     }
 }
 
